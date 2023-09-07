77fb6ba4649c7ae986a5f4aaa60387d80f4c79b8
NicholasDaviesNZ <96546397+NicholasDaviesNZ@users.noreply.github.com>
2023-09-07 17:04:51 +1200

    Update align.py

--- traitor/cli/align.py
+++ traitor/cli/align.py
@@ -271,8 +271,9 @@ def align(
 
     # mask_dir is a simple directory containing mask images.
     else:
+        print(list(mask_dir.rglob(f"{image_name}_mask.png"))[0])
         mask_files = np.array(
-            [mask_dir / list(mask_dir.glob(f"**{image_name}_mask.png"))[0] for image_name in image_names]
+            [mask_dir / list(mask_dir.rglob(f"{image_name}_mask.png"))[0] for image_name in image_names]
         )
         mask_files_matching = np.array([f.exists() for f in mask_files])
         if mask_files_matching.sum() < 1:
